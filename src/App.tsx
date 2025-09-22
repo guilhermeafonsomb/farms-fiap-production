3e5bce4ef5e4df1812becbf8915d010d6271f6b0
Guilherme Afonso <guilhermeafonsomb@gmail.com>
2025-09-22 14:26:30 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container mx-auto mt-8 p-4 bg-red-500 rounded-lg shadow-md">
+      <h1 className=" text-3xl font-bold text-blue-800">Production!</h1>
+      <p className="text-blue-600">Production</p>
+    </div>
+  );
+}
+
+export default App;
